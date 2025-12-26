89792ef9ae4c683fad57889c0fb89acd43fed13f
gopinadh <gopinadh@LAPTOP-JSG6M7F3.bbrouter>
2025-12-26 15:30:26 +0530

    Day4

--- src/com/tnsif/intro/constructors/constructoroverloading/Employee.java
+++ src/com/tnsif/intro/constructors/constructoroverloading/Employee.java
@@ -7,22 +7,22 @@ public class Employee {
 		System.out.println("Ready To Display Employee Information ");
 	}
 	Employee(String empName){
-		System.out.println("=== 1-Parameter Construcor ===");
+		System.out.println("=== 1-Parameter Constructor ===");
 		System.out.println("Employee name = "+empName);
 	}
 	Employee(String empName,int empId){
-		System.out.println("=== 2-Parameters Construcor ===");
+		System.out.println("=== 2-Parameters Constructor ===");
 		System.out.println("Employee name = "+empName);
 		System.out.println("Employee id = "+empId);
 	}
 	Employee(String empName,int empId,double empSal){
-		System.out.println("=== 3-Parameters Construcor ===");
+		System.out.println("=== 3-Parameters Constructor ===");
 		System.out.println("Employee name = "+empName);
 		System.out.println("Employee id = "+empId);
 		System.out.println("Employee salary = "+empSal);
 	}
 	Employee(String empName,int empId,double empSal,String empAddr){
-		System.out.println("=== 4-Parameters Construcor ===");
+		System.out.println("=== 4-Parameters Constructor ===");
 		System.out.println("Employee name = "+empName);
 		System.out.println("Employee id = "+empId);
 		System.out.println("Employee salary = "+empSal);
