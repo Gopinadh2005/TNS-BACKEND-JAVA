b283001c05e30140fecf3d40eeb6f00243e186ac
gopinadh <gopinadh@LAPTOP-JSG6M7F3.bbrouter>
2026-01-01 21:21:37 +0530

    Day7

---
+++ src/com/tnsif/intro/wrapper/Autoboxing.java
@@ -0,0 +1,14 @@
+package com.tnsif.intro.wrapper;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		int a=10;
+		Integer i=new Integer(a);
+		System.out.println("a="+a);
+		float b=20.5f;
+		Float f=b;
+		System.out.println("f="+f);
+	}
+
+}
---
+++ src/com/tnsif/intro/wrapper/Autounboxing.java
@@ -0,0 +1,14 @@
+package com.tnsif.intro.wrapper;
+
+public class Autounboxing {
+
+	public static void main(String[] args) {
+		Integer a=10;
+		int i=a;
+		System.out.println("a="+a);
+		Float b=20.5f;
+		float f=b;
+		System.out.println("f="+f);
+	}
+
+}
