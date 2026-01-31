75200c8709008bd10696bd53aa072c94f0e8a431
Gopinadh2005 <makkalagopinadh@gmail.com>
2026-01-31 14:38:08 +0530

    Junit

---
+++ src/com/tnsif/junit/Calculations.java
@@ -0,0 +1,13 @@
+package com.tnsif.junit;
+
+public class Calculations {
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public int divide(int a,int b) {
+		return a/b;
+	}
+	public String getName() {
+		return "Junit";
+	}
+}
